f3327948183eab21fce2e95e8fb6fed9a5f20868
hassa357 <hassa357@morris.umn.edu>
2018-09-28 22:52:35 -0500

    Update the .c file for mergesort.

--- mergesort/mergesort.c
+++ mergesort/mergesort.c
@@ -4,35 +4,33 @@
 #include "mergesort.h"
 
 
-public void mergesort(int size, int[] values) {
+void mergesort(int size, int[] values) {
     mergesortRange(values, 0, values.length);
   }
 
-  private void mergesortRange(int[] values, int startIndex, int endIndex) {
-    int rangeSize = endIndex - startIndex;
-    if (needsSorting(rangeSize)) {
-      int midPoint = (startIndex + endIndex) / 2;
-      mergesortRange(values, startIndex, midPoint);
-      mergesortRange(values, midPoint, endIndex);
-      mergeRanges(values, startIndex, midPoint, endIndex);
+  void mergesortRange(int[] values, int startIndex, int endIndex) {
+    
+	  int rangeSize = endIndex - startIndex;
+    
+	  if (needsSorting(rangeSize)) {
+             int midPoint = (startIndex + endIndex) / 2;
+             mergesortRange(values, startIndex, midPoint);
+             mergesortRange(values, midPoint, endIndex);
+             mergeRanges(values, startIndex, midPoint, endIndex);
     }
   }
 
-  private void mergeRanges(int[] values, int startIndex, int midPoint,
-  int endIndex) {
-    /*
-     * Assume that the two ranges are sorted:
-     *   (forall i | startIndex <= i <= j < midPoint : values[i] <= values[j])
-     *   (forall i | midPoint <= i <= j < endIndex : values[i] <= values[j])
-     * then merge them into a single sorted array, copy that back, and return.
-     */
-    final int rangeSize = endIndex - startIndex;
-    int[] destination = new int[rangeSize];
+  void mergeRanges(int[] values, int startIndex, int midPoint, int endIndex) {
+   
+    int rangeSize = endIndex - startIndex;
+    int* destination = (int*) calloc(rangeSize, sizeOf(int));
     int firstIndex = startIndex;
     int secondIndex = midPoint;
     int copyIndex = 0;
+   
     while (firstIndex < midPoint && secondIndex < endIndex) {
-      if (values[firstIndex] < values[secondIndex]) {
+      
+	    if (values[firstIndex] < values[secondIndex]) {
         destination[copyIndex] = values[firstIndex];
         ++firstIndex;
       } else {
@@ -41,22 +39,26 @@ public void mergesort(int size, int[] values) {
       }
       ++copyIndex;
     }
+    
     while (firstIndex < midPoint) {
       destination[copyIndex] = values[firstIndex];
       ++copyIndex;
       ++firstIndex;
     }
+    
     while (secondIndex < endIndex) {
       destination[copyIndex] = values[secondIndex];
       ++copyIndex;
       ++secondIndex;
     }
+    
     for (int i = 0; i < rangeSize; ++i) {
       values[i + startIndex] = destination[i];
     }
+    free(destination);
   }
 
-  private boolean needsSorting(int rangeSize) {
+  bool needsSorting(int rangeSize) {
     return rangeSize >= 2;
   }
 
