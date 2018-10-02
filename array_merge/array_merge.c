b14126de5c4f725ad3e2c24efa93dd4e27ac5d31
hassa357 <hassa357@morris.umn.edu>
2018-10-01 21:34:10 -0500

    Add array_merge.c file.

---
+++ array_merge/array_merge.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+#include "array_merge.h"
+
+
+int* array_merge(int num_arrays, int* sizes, int** values);
+
+// Need to finish writing code.
